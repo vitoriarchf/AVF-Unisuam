5e770e4309ddee4727ef39eb6aeb8588a9c0a1fb
Vitória Rocha <contatovitoriarrocha@gmail.com>
2025-10-01 23:35:39 -0300

    Alterações no texto

--- src/app/api/api.page.ts
+++ src/app/api/api.page.ts
@@ -3,33 +3,34 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton } from '@ionic/angular/standalone';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-api',
   templateUrl: './api.page.html',
   styleUrls: ['./api.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonButton, CommonModule, FormsModule, HttpClientModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonButton, CommonModule, FormsModule, HttpClientModule, RouterModule]
 })
 export class ApiPage implements OnInit {
-  joke: any = null;
+  catFact: string = '';
   loading = false;
 
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
-    this.getJoke();
+    this.getCatFact();
   }
 
-  getJoke() {
+  getCatFact() {
     this.loading = true;
-    this.http.get('https://official-joke-api.appspot.com/random_joke').subscribe({
+    this.http.get<any>('https://catfact.ninja/fact').subscribe({
       next: (data) => {
-        this.joke = data;
+        this.catFact = data.fact;
         this.loading = false;
       },
       error: () => {
-        this.joke = { setup: 'Erro ao buscar piada.', punchline: '' };
+        this.catFact = 'Erro ao buscar fato.';
         this.loading = false;
       }
     });
